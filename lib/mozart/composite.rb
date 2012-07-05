acc1fc2dc3bd158d90e263f15b1ad74bb2e71a07
Gregory Brown <gregory.t.brown@gmail.com>
2012-07-05 10:33:09 -0400

    Implement trivial dispatch

--- lib/mozart/composite.rb
+++ lib/mozart/composite.rb
@@ -12,6 +12,11 @@ module Mozart
       parts.any? { |part| part.respond_to?(message) }
     end
 
+    def dispatch(message)
+      parts.find { |part| part.respond_to?(message) }
+           .public_send(message)
+    end
+
     private
 
     attr_reader :parts
--- test/composite_test.rb
+++ test/composite_test.rb
@@ -4,8 +4,8 @@ require_relative "../lib/mozart/composite"
 
 describe Mozart::Composite do
   let(:parts) do
-    { :a => Struct.new(:bar).new(:returned_by_bar),
-      :b => Struct.new(:foo).new(:returned_by_foo) }
+    { :a => Struct.new(:foo).new(:returned_by_foo),
+      :b => Struct.new(:bar).new(:returned_by_bar) }
   end
 
   it "knows what messages it can receive" do
@@ -16,5 +16,14 @@ describe Mozart::Composite do
     assert composite.receives?(:foo), "Expected composite to receive foo"
     assert composite.receives?(:bar), "Expected composite to receive bar"
     refute composite.receives?(:baz), "Did not expect composite to receive baz"
+  end
+
+  it "dispatches messages to its parts" do
+    composite = Mozart::Composite.new
+
+    parts.values.each { |part| composite << part }
+
+    composite.dispatch(:foo).must_equal(:returned_by_foo)
+    composite.dispatch(:bar).must_equal(:returned_by_bar)
   end
 end
