3c8ce9acfa1ab65d48af863dabf11840642c6853
Gregory Brown <gregory.t.brown@gmail.com>
2012-07-05 11:31:47 -0400

    Add value object

--- lib/mozart/value.rb
+++ lib/mozart/value.rb
@@ -1,4 +1,22 @@
 module Mozart
-  class Value
+  def self.value(*field_names)
+    Class.new do
+      define_method(:initialize) do |params={}|
+        raise ArgumentError unless (params.keys - field_names).empty?
+
+        self.data = {}
+
+        params.each { |k,v| data[k] = v }
+      end
+
+      field_names.each do |name|
+        define_method(name) { data[name] }
+        define_method("#{name}=") { |v| data[name] = v }
+      end
+
+      private
+
+      attr_accessor :data
+    end
   end
 end
--- test/value_test.rb
+++ test/value_test.rb
@@ -1,6 +1,19 @@
 require "minitest/autorun"
 require_relative "../lib/mozart/value"
 
-describe Mozart::Value do
+describe "Mozart.value" do
+  it "produces Struct-like classes" do
+    Position = Mozart.value(:x, :y)
 
+    pos1 = Position.new(:x => 10, :y => 20)
+    pos1.x.must_equal(10)
+    pos1.y.must_equal(20)
+
+    pos2   = Position.new
+    pos2.x = 3
+    pos2.y = 5
+
+    pos2.x.must_equal(3)
+    pos2.y.must_equal(5)
+  end
 end
